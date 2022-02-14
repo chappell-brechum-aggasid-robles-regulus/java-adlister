f547654328d72d22e3109b0e7716c551cd4901c1
Jonathan Robles <jonathan.robles287@gmail.com>
2022-02-14 15:38:04 -0600

    Added searchAdByTitle() to Ads.java and ListAdsDao.java

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -9,4 +9,6 @@ public interface Ads {
     List<Ad> all();
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
+    // Search for a List of Ads by Title
+    List<Ad> searchAdByTitle(String param);
 }
