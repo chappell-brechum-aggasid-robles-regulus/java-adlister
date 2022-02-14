8a7f223dc668d8ac63911d4058aeb635682a72a0
adamchappell00 <adam.chappell00@gmail.com>
2022-02-14 16:13:37 -0600

    edit and delete user buttons added, need functions, dispay ads by user complete, needs refactor

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +13,22 @@ import java.io.IOException;
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("user") == null) {
+        User currentUser = (User) request.getSession().getAttribute("user");
+        if (currentUser == null) {
             response.sendRedirect("/login");
             return;
+        } else {
+            String username = currentUser.getUsername();
+            String email = currentUser.getEmail();
+            int id = (int) currentUser.getId();
+            request.getSession().setAttribute("username", username);
+            request.getSession().setAttribute("email", email);
+            request.getSession().setAttribute("id", id);
+            request.getSession().setAttribute("ads", DaoFactory.getAdsDao().all());
         }
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
     }
 }
