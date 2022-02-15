6f3099e25a1a78890f0e000934670673d1d40ec7
adamchappell00 <adam.chappell00@gmail.com>
2022-02-15 13:41:18 -0600

    Password Reset Servlet doPost set up, needs statement-function added to UserDAO

--- src/main/java/com/codeup/adlister/controllers/PasswordServlet.java
+++ src/main/java/com/codeup/adlister/controllers/PasswordServlet.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,6 +25,27 @@ public class PasswordServlet extends HttpServlet {
             request.getRequestDispatcher("/WEB-INF/reset.jsp").forward(request, response);
         }
     }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User currentUser = (User) request.getSession().getAttribute("user");
+        String oldpassword = request.getParameter("oldpassword");
+        String newpassword = request.getParameter("newpassword");
+        String passwordConfirmation = request.getParameter("confirm_password");
+        boolean incorrectPass = Password.check(oldpassword, currentUser.getPassword());
+        boolean inputHasErrors = oldpassword.isEmpty()
+                || newpassword.isEmpty()
+                || (! newpassword.equals(passwordConfirmation));
+        if(incorrectPass){
+            // Add Error Message, Password Incorrect
+            response.sendRedirect("/reset");
+        } else if (inputHasErrors){
+            // Add Error Message, New Password and Confirmation do not match
+            response.sendRedirect("/reset");
+        } else {
+            // Add usersDAO function to change password
+            // Add Success Message
+            response.sendRedirect("/reset");
+        }
 
+    }
 
 }
